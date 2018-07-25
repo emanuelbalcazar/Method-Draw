e86fc079bda0f9c1fe7120ac33fb87b68771b394
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2018-07-25 18:01:43 -0300

    Conexión a MQTT con topico "/md/#"

--- editor/src/mqtt.js
+++ editor/src/mqtt.js
@@ -1,11 +1,11 @@
-const TOPIC = '';
+const TOPIC = '/md/#';
 
 // genero un ID de cliente aleatorio para permitir multiples conexiones.
 const clientId = parseInt(Math.random() * 100, 10);
 console.log('[mqtt] - cliente con id %s generado', clientId);
 
-/* // creo un cliente MQTT.
-const client = new Paho.MQTT.Client(window.location.host, 80, "/mqtt", clientId.toString());
+// creo un cliente MQTT.
+const client = new Paho.MQTT.Client('localhost', 80, '/mqtt', clientId.toString());
 
 // inicio la conexion, y me subscribo al topico definido en la constante.
 client.connect({
@@ -21,7 +21,24 @@ client.connect({
             }
         })
     },
-    onFailure: () => {
-        console.log('[MQTT] - Fallo la conexion a mqtt');
+    onFailure: (err) => {
+        console.log('[MQTT] - Fallo la conexion a mqtt', err);
     }
-}); */
+});
+
+client.onMessageArrived = (message) => {
+    console.log('[Method Draw] - Llego un nuevo mensaje:', message);
+    let selection = d3.select("svg").selectAll('[topic="' + message.topic + '"]');
+    let params = JSON.parse(message.payloadString);
+    
+    console.log('[Method Draw] - selection:', selection);
+
+    if (selection) {
+
+        if (params.selectorId)
+            selection = selection.select("#" + params.selectorId);
+
+        selection.styles(params.styles);
+        selection.attrs(params.attrs);
+    }
+}
